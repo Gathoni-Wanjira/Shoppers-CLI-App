97ad2cc8f7c99e29990a5d34a420531b9766383d
Gathoni <janetgathoni910@gmail.com>
2023-09-07 20:54:24 +0300

    Made model revisions

---
+++ lib/migrations/versions/987551e03799_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 987551e03799
+Revises: 
+Create Date: 2023-09-07 19:35:29.383490
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '987551e03799'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- lib/model/models.py
+++ lib/model/models.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///shoppers.db')
 
 
 Base = declarative_base()
@@ -12,6 +15,11 @@ class Customer(Base):
     last_name = Column(String(), index = True)
     
     
+    def __repr__(self):
+        return f"Customer {self.id}: " \
+            + f"{self.first_name}, " \
+            + f"{self.last_name}, "
+    
 class Product(Base):
     __tablename__ = 'products'
 
@@ -21,6 +29,14 @@ class Product(Base):
     product_serialNo = Column(Integer())
     
     
+     
+    def __repr__(self):
+        return f"Product {self.id}: " \
+            + f"{self.product_name}, " \
+            + f"{self.price}, " \
+            + f"{self.product_serialNo}, "
+    
+    
 class Cart(Base):
     __tablename__ = 'carts'
 
@@ -28,4 +44,11 @@ class Cart(Base):
     
     product_id = Column(Integer(), ForeignKey('products.id'))
     customer_id = Column(Integer(), ForeignKey('customers.id'))
+    
+    
+    def __repr__(self):
+        return f"Cart {self.id}: " \
+            + f"{self.product_id}, " \
+            + f"{self.customer_id}, "
+ 
  
